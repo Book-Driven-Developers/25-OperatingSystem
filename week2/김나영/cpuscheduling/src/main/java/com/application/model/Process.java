c6e4a1276e76645a505a4cc613ed478347930756
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-12-17 22:46:02 +0900

    feat(scheduler) : RoundRobinScheduler

--- week2/김나영/cpuscheduling/src/main/java/com/application/model/Process.java
+++ week2/김나영/cpuscheduling/src/main/java/com/application/model/Process.java
@@ -1,11 +1,11 @@
 package com.application.model;
 
 public class Process {
-    int pid; // 프로세스 ID
-    int burstTime; // CPU 실행 시간
-    int arrivalTime; // 도착 시간
-    int remainingTime; // 남은 실행 시간
-    int completionTime; // 완료 시간
+    public int pid; // 프로세스 ID
+    public int burstTime; // CPU 실행 시간
+    public int arrivalTime; // 도착 시간
+    public int remainingTime; // 남은 실행 시간
+    public int completionTime; // 완료 시간
 
     public Process(int pid, int burstTime, int arrivalTime) {
         this.pid = pid;
--- week2/김나영/cpuscheduling/src/main/java/com/application/scheduler/Scheduler.java
+++
@@ -1,7 +0,0 @@
-package com.application.scheduler;
-
-import java.util.List;
-
-public interface Scheduler {
-    void schedule(List<Process> processes);
-}
